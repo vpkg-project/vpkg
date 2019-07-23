37657837a7f11c45d14918660dcc01392641320c
Ned Palacios <npdoesmc@gmail.com>
2019-07-23 22:45:32 +0800

    removing packages works

--- commands.v
+++ commands.v
@@ -26,18 +26,41 @@ fn install_packages(global bool) {
     return
 }
 
-// TODO
-// fn remove_packages(packages []string) {
-//     for package in packages {
-//         //
-//     }
-// }
+fn remove_packages(packages []string) {
+    mut removed_packages := []string
 
-// fn update_packages() {
-//     // TODO: Update packages
-// }
+    for package in packages {
+        pkg_name := if package.starts_with('v-') { package.all_after('v-') } else { package }
+        status := delete_package_contents('${ModulesDir}/${pkg_name}')
 
-fn get_packages(packages []string, global bool) []DownloadedPackage {
+        if status {
+            removed_packages << package
+        }
+    }
+
+    println('${removed_packages.len} packages were removed.')
+}
+
+// TODO: Update packages
+fn update_packages() {
+    // readlockfile
+    installed_packages := os.ls(ModulesDir)
+    println('Updating packages')
+
+    for package in installed_packages {
+        current_hash := os.exec('git log --pretty=format:\'%h\' -n 1')
+        mut latest_hash := current_hash
+
+        os.exec('git fetch')
+        latest_hash = os.exec('git log --pretty=format:\'%h\' -n 1')
+
+        if current_hash != latest_hash {
+            os.exec('git pull')
+        }
+    }
+}
+
+fn get_packages(packages []string, global bool) {
     mut installed_packages := []DownloadedPackage
 
     for i := 0; i < packages.len; i++ {
@@ -46,7 +69,7 @@ fn get_packages(packages []string, global bool) []DownloadedPackage {
         installed_packages << package
     }
 
-    return installed_packages
+    println('${installed_packages.len} packages were installed successfully.')
 }
 
 fn show_version() {
@@ -60,8 +83,9 @@ fn show_help() {
 
     println('\nCOMMANDS\n')
 
-    println('get [packages]                     Fetch packages from the registry or the git repo.')
-    println('install                            reads the ".vpkg.json" file and installs the necessary packages.')
+    println('get [packages]                     Fetch and installs packages from the registry or the git repo.')
+    println('install                            Reads the ".vpkg.json" file and installs the necessary packages.')
+    println('remove [packages]                  Removes packages')
     println('help                               Prints this help message.')
     println('version                            Prints the Version of this program.')
 
