e10530c5371a7971afe9dc71c167970cf69e17d0
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-02-12 19:59:55 +0800

    Using `show` word instead of prints

--- api/commands.v
+++ api/commands.v
@@ -254,7 +254,7 @@ fn (vpkg Vpkg) show_help() {
     println('\nCOMMANDS\n')
 
     println('get [packages]                             Fetch and installs packages from the registry or the git repo.')
-    println('help                                       Prints this help message.')
+    println('help                                       Show this help message.')
     println('info                                       Show project\'s package information.')
     println('init                                       Creates a package manifest file into the current directory. Defaults to "vpkg".')
     println('install                                    Reads the package manifest file and installs the necessary packages.')
@@ -263,7 +263,7 @@ fn (vpkg Vpkg) show_help() {
     println('remove [packages]                          Removes packages')
     println('test                                       Tests the current lib/app.')
     println('update                                     Updates the packages.')
-    println('version                                    Prints the version of this program.')
+    println('version                                    Show the version of this program.')
 
     println('\nOPTIONS\n')
     println('--files [file1,file2]                      Specifies other locations of test files (For "test" command)')
